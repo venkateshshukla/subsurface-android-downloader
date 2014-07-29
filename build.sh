b3f62aa1368b587184c58e35e700f8275d28395c
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2014-07-30 02:53:16 +0530

    Added script to build dependencies
    This project requires presence of libusb, libftdi and libdivecomputer libraries
    on the system. Hence, these are cross compiled for usage on android. The
    generated static libraries can then be used for JNI interfacing.
    This script assumes presence of libdivecomputer, libusb and libftdi submodules
    in crossbuild folder.
    To make, run the command as per target arch : bash build.sh [x86 / arm]
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

---
+++ build.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+set -e
+
+# Configure where we can find things here
+export ANDROID_NDK_ROOT=/android_ndk
+export ANDROID_SDK_ROOT=/android_sdk
+
+# Platform architecture
+export ARCH=${1-x86}
+
+if [ "$ARCH" = "arm" ] ;
+then
+	BUILDCHAIN=arm-linux-androideabi
+	export TARGET_ARCH_ABI=armeabi-v7a
+elif [ "$ARCH" = "x86" ] ;
+then
+	BUILDCHAIN=i686-linux-android
+	export TARGET_ARCH_ABI=x86
+fi
+
+pushd crossbuild
+
+# Initialise cross toolchain
+if [ ! -e ndk-$ARCH ] ; then
+	$ANDROID_NDK_ROOT/build/tools/make-standalone-toolchain.sh --arch=$ARCH --install-dir=ndk-$ARCH --platform=android-19
+fi
+
+# Declare necessary variables for cross compilation
+export BUILDROOT=$PWD
+export PATH=${BUILDROOT}/ndk-$ARCH/bin:$PATH
+export PREFIX=${BUILDROOT}/ndk-$ARCH/sysroot/usr
+export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
+export CC=${BUILDCHAIN}-gcc
+export CXX=${BUILDCHAIN}-g++
+export AR=${BUILDCHAIN}-ar
+
+# Download libdivecomputer, libusb and libftdi submodule
+if [ ! -e libdivecomputer/configure.ac ] || [ ! -e libusb/configure.ac ] || [ ! -e libftdi/CMakeLists.txt ] ; then
+	git submodule init
+	git submodule update
+fi
+
+# Prepare for building
+mkdir -vp build
+
+# Build libusb
+if [ ! -e libusb/configure ] ; then
+	pushd libusb
+	autoreconf --install
+	popd
+fi
+
+mkdir -p build/libusb-build-$ARCH
+pushd build/libusb-build-$ARCH
+if [ ! -e Makefile ] ; then
+	../../libusb/configure --host=${BUILDCHAIN} --prefix=${PREFIX} --enable-static --disable-shared --disable-udev
+fi
+make
+make install
+popd
+
+# Build libftdi
+mkdir -p build/libftdi-build-$ARCH
+pushd build/libftdi-build-$ARCH
+if [ ! -e Makefile ] ; then
+	cmake -DCMAKE_C_COMPILER=${CC} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_PREFIX_PATH=${PREFIX} -DBUILD_SHARED_LIBS=OFF -DSTATICLIBS=ON -DPYTHON_BINDINGS=OFF -DDOCUMENTATION=OFF -DFTDIPP=OFF ../../libftdi
+fi
+make
+make install
+popd
+
+# Build libdivecomputer
+if [ ! -e libdivecomputer/configure ] ; then
+	pushd libdivecomputer
+	autoreconf -i
+	popd
+fi
+
+mkdir -p build/libdivecomputer-build-$ARCH
+pushd build/libdivecomputer-build-$ARCH
+if [ ! -e Makefile ] ; then
+	../../libdivecomputer/configure --host=${BUILDCHAIN} --prefix=${PREFIX} --enable-static --disable-shared --enable-logging LDFLAGS=-llog
+fi
+make
+make install
+popd
+
+popd # from crossbuild
+
+if [[ $? == 0 ]] ; then
+echo "Finished building requisites."
+fi
+
