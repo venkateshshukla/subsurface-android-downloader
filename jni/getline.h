323081c3dfe6478cbf16f53ba3e3716c6fc1f062
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2014-08-03 18:16:25 +0530

    Removed all subsurface files from jni
    Subsurface would be compiled as a submodule on its own. It is present in
    crossbuild/subsurface. Hence, all the files are removed which would be directly
    taken from the submodule.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- jni/getline.h
+++
@@ -1,8 +0,0 @@
-#ifndef GETLINE_H
-#define GETLINE_H
-
-#include <stdio.h>
-
-ssize_t getline(char **, size_t *, FILE *);
-
-#endif
--- jni/ssrf-version.h
+++
@@ -1,1 +0,0 @@
-#define VERSION_STRING "4.1.90-181-ga9aabe3f27fe"
