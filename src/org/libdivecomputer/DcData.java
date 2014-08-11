0ca9b8b31c815aa13e4e9e88f6b97db2b9deb2ca
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2014-08-11 22:18:13 +0530

    Checks in DcData for proper input
    DcData should contain values in particular form.
    For ex. if vendor or product name is empty, its an error. Similarly if outfile
    or dumpfile is empty, and it is needed, its an error. Similar is the case of non
    positive file descriptor for opened usb device.
    Added checks for these. Any of these conditions is attempted will result in
    exception being thrown.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- src/org/libdivecomputer/DcData.java
+++ src/org/libdivecomputer/DcData.java
@@ -32,6 +32,7 @@ public class DcData implements Parcelable {
         public void nativeResetDcData() {
                 resetDcData();
         }
+
         public void nativeSetUsbFd(int usbFd) throws DcException {
                 int ret = setUsbFd(this.fd);
                 if (ret == -1)
@@ -46,7 +47,6 @@ public class DcData implements Parcelable {
                         case -2 :
                                 throw new DcException("Null Filename");
                 }
-
         }
 
         public void nativeSetDumpFile(String flname) throws DcException {
@@ -57,7 +57,6 @@ public class DcData implements Parcelable {
                         case -2 :
                                 throw new DcException("Null Filename");
                 }
-
         }
 
         public void nativeSetXmlFile(String flname) throws DcException {
@@ -83,6 +82,7 @@ public class DcData implements Parcelable {
                         throw new DcException("No device found with given information.");
                 }
         }
+
         public void nativeInterruptImport() throws DcException {
                 int ret = interruptImport();
                 if (ret != 0) {
@@ -101,7 +101,10 @@ public class DcData implements Parcelable {
                 return fd;
         }
 
-        public void setFd(int fd) {
+        public void setFd(int fd) throws DcException {
+                if (fd <= 0) {
+                        throw new DcException("Usb File Descriptor is invalid.");
+                }
                 this.fd = fd;
         }
 
@@ -109,15 +112,22 @@ public class DcData implements Parcelable {
                 return vendor;
         }
 
-        public void setVendor(String vendor) {
+        public void setVendor(String vendor) throws DcException {
+                // This should never happen
+                if (vendor.equals(null) || vendor.isEmpty()) {
+                        throw new DcException("Empty vendor string is not allowed.");
+                }
                 this.vendor = vendor;
         }
-
         public String getProduct() {
                 return product;
         }
 
-        public void setProduct(String product) {
+        public void setProduct(String product) throws DcException {
+                // This should never happen
+                if (product.equals(null) || product.isEmpty()) {
+                        throw new DcException("Empty product string is not allowed.");
+                }
                 this.product = product;
         }
 
@@ -157,7 +167,10 @@ public class DcData implements Parcelable {
                 return logfilepath;
         }
 
-        public void setLogfilepath(String logfilepath) {
+        public void setLogfilepath(String logfilepath) throws DcException {
+                if (logfilepath.equals(null) || logfilepath.isEmpty()) {
+                        throw new DcException("Empty logfile string.");
+                }
                 this.logfilepath = logfilepath;
         }
 
@@ -165,7 +178,10 @@ public class DcData implements Parcelable {
                 return outfilepath;
         }
 
-        public void setOutfilepath(String dumpfilepath) {
+        public void setOutfilepath(String dumpfilepath) throws DcException {
+                if (dumpfilepath.equals(null) || dumpfilepath.isEmpty()) {
+                        throw new DcException("Empty output file string.");
+                }
                 this.outfilepath = dumpfilepath;
         }
 
