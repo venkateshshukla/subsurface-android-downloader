727f74d18457bf0e53e1610aff69924ae31ae990
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2014-08-11 20:02:28 +0530

    Huge changes in architecture.
    Made significant changes to the architecture of the application.
    Rather than doing all the stuff at once in a single call (init_dc_data), we will
    now have individual calls to the set functions. This will give better control
    over the java side of the application. JNI wrappers are hence more sensible now
    with well defined tasks.
    
    Also added process_dives and save_dives function calls as these would be
    necessary for import.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- jni/subsurface_jni.c
+++ jni/subsurface_jni.c
@@ -25,28 +25,181 @@
 
 static device_data_t dcdata;
 
-void device_descriptor_lookup(device_data_t *d)
+static char *xmlfile_name;
+static bool prefer_downloaded;
+
+// Reset the dcdata
+void reset_dcdata(JNIEnv *env, jobject jobj)
 {
-	dc_iterator_t *iterator = NULL;
-	dc_descriptor_t *descriptor = NULL;
-	struct mydescriptor *mydescriptor;
-
-	dc_descriptor_iterator(&iterator);
-	while (dc_iterator_next(iterator, &descriptor) == DC_STATUS_SUCCESS) {
-		const char *vendor = dc_descriptor_get_vendor(descriptor);
-		const char *product = dc_descriptor_get_product(descriptor);
-		if (!strcmp(vendor, d->vendor) && !strcmp(product, d->product)) {
-			LOGD("DC Descriptor found : %s / %s\n", vendor, product);
-			d->descriptor = descriptor;
-			break;
-		}
-	}
-	dc_iterator_free(iterator);
-
+	LOG_F("reset_dcdata");
+	memset(&dcdata, 0, sizeof (device_data_t));
 }
 
-JNIEXPORT void JNICALL get_device_map(JNIEnv *env, jobject jobj, jobject jhashMap)
+// Set the usb file descriptor of dcdata
+int set_usb_fd(JNIEnv *env, jobject jobj, jint usb_fd)
 {
+	LOG_F("set_usb_fd");
+	if (usb_fd > 0) {
+		dcdata.fd = usb_fd;
+		return 0;
+	} else {
+		return -1; // Invalid file descriptor
+	}
+}
+
+// Prefer downloaded dives or already existing
+void set_prefer(JNIEnv *env, jobject jobj, jboolean jprefer)
+{
+	LOG_F("set_prefer");
+        prefer_downloaded = jprefer;
+}
+
+// Force download of dives even when they are already present?
+void set_force(JNIEnv *env, jobject jobj, jboolean jforce)
+{
+	LOG_F("set_force");
+        dcdata.force_download = jforce;
+}
+
+// Should logging be done?
+void set_log(JNIEnv *env, jobject jobj, jboolean jlog)
+{
+	LOG_F("set_log");
+        dcdata.libdc_log = jlog;
+}
+
+// Should file dumping be done?
+void set_dump(JNIEnv *env, jobject jobj, jboolean jdump)
+{
+	LOG_F("set_dump");
+        dcdata.libdc_dump = jdump;
+}
+
+// Set the vendor name
+int set_vendor_name(JNIEnv *env, jobject jobj, jstring jvendor)
+{
+	LOG_F("set_vendor_name");
+	if (jvendor != NULL) {
+		const char *vn = (*env)->GetUTFChars(env, jvendor, NULL);
+		jsize len = (*env)->GetStringUTFLength(env, jvendor);
+		char *vendor = (char *) malloc(len + 1);
+		if (product == NULL)
+			return -1; // Memory error
+		strncpy(vendor, vn, len);
+		(*env)->ReleaseStringUTFChars(env, jvendor, vn);
+		dcdata.vendor = vendor;
+		return 0;
+	}
+	return -2; // Null filename
+}
+
+// Set product name
+int set_product_name(JNIEnv *env, jobject jobj, jstring jproduct)
+{
+	LOG_F("set_product_name");
+	if (jproduct != NULL) {
+		const char *pr = (*env)->GetUTFChars(env, jproduct, NULL);
+		jsize len = (*env)->GetStringUTFLength(env, jproduct);
+		char *product = (char *) malloc(len + 1);
+		if (product == NULL)
+			return -1; // Memory error
+		strncpy(product, pr, len);
+		(*env)->ReleaseStringUTFChars(env, jproduct, pr);
+		dcdata.product = product;
+		return 0;
+	}
+	return -2; // Null filename
+}
+
+// Set the logfile and
+int  set_logfile(JNIEnv *env, jobject jobj, jstring jfilename)
+{
+	LOG_F("set_logfile");
+	if (jfilename != NULL) {
+		const char *lf = (*env)->GetUTFChars(env, jfilename, NULL);
+		jsize len = (*env)->GetStringUTFLength(env, jfilename);
+		char *logfile = (char *) malloc(len + 1);
+		if (logfile == NULL)
+			return -1; // Memory error
+		strncpy(logfile, lf, len);
+		(*env)->ReleaseStringUTFChars(env, jfilename, lf);
+		logfile_name = logfile;
+		return 0;
+	}
+	return -2; // Null filename
+}
+
+// Set the dumpfile
+int  set_dumpfile(JNIEnv *env, jobject jobj, jstring jfilename)
+{
+	LOG_F("set_dumpfile");
+	if (jfilename != NULL) {
+		const char *df = (*env)->GetUTFChars(env, jfilename, NULL);
+		jsize len = (*env)->GetStringUTFLength(env, jfilename);
+		char *dumpfile = (char *) malloc(len + 1);
+		if (dumpfile == NULL)
+			return -1; // Memory error
+		strncpy(dumpfile, df, len);
+		(*env)->ReleaseStringUTFChars(env, jfilename, df);
+		dumpfile_name = dumpfile;
+		return 0;
+	}
+	return -2; // Null filename
+}
+
+// Set the xmlfile
+int  set_xmlfile(JNIEnv *env, jobject jobj, jstring jfilename)
+{
+	LOG_F("set_xmlfile");
+	if (jfilename != NULL) {
+		const char *xf = (*env)->GetUTFChars(env, jfilename, NULL);
+		jsize len = (*env)->GetStringUTFLength(env, jfilename);
+		char *xmlfile = (char *) malloc(len + 1);
+		if (xmlfile == NULL)
+			return -1; // Memory error
+		strncpy(xmlfile, xf, len);
+		(*env)->ReleaseStringUTFChars(env, jfilename, xf);
+		xmlfile_name = xmlfile;
+		return 0;
+	}
+	return -2; // Null filename
+}
+
+// Start importing data from Divecomputer.
+void do_dc_import(JNIEnv *env, jobject jobj)
+{
+	LOG_F("do_dc_import");
+	const char *error_text;
+	error_text = do_libdivecomputer_import(&dcdata);
+	if (error_text)
+		LOGD("error while import : %s\n", error_text);
+	LOGD("Finished do_dc_import\n");
+}
+
+// After download if the download is successful, start processing the dives.
+void do_process_dives(JNIEnv *env, jobject jobj)
+{
+        LOG_F("do_process_dives");
+        // TODO insert check for cancelled thread.
+        process_dives(true, prefer_downloaded);
+}
+
+// Save the dives after processing them.
+int do_save_dives(JNIEnv *env, jobject jobj)
+{
+        LOG_F("do_save_dives");
+        int ret = save_dives(xmlfile_name);
+        if (ret == -1)
+                LOGD("Error saving the dives.\n");
+        // Do some error checking as well. And return the error to user.
+        return ret;
+}
+
+// Iterate through all the devices and generate a java HashMap<String,
+// ArrayList<String>> containing all the Vendors and Products.
+void  get_device_map(JNIEnv *env, jobject jobj, jobject jhashMap)
+{
+	LOG_F("get_device_map");
 	jclass HashMap = (*env)->GetObjectClass(env, jhashMap);
 	jmethodID hmInit = (*env)->GetMethodID(env, HashMap, "<init>", "()V");
 	jmethodID containsKey = (*env)->GetMethodID(env, HashMap, "containsKey", "(Ljava/lang/Object;)Z");
@@ -62,7 +215,6 @@ JNIEXPORT void JNICALL get_device_map(JNIEnv *env, jobject jobj, jobject jhashMa
 
 	dc_iterator_t *iterator = NULL;
 	dc_descriptor_t *descriptor = NULL;
-	struct mydescriptor *mydescriptor;
 
 	bool isPresent;
 	dc_descriptor_iterator(&iterator);
@@ -85,88 +237,9 @@ JNIEXPORT void JNICALL get_device_map(JNIEnv *env, jobject jobj, jobject jhashMa
 	dc_iterator_free(iterator);
 }
 
-JNIEXPORT void JNICALL init_dcdata(JNIEnv *env, jobject jobj, jobject jdcdata)
-{
-	LOG_F("init_dcdata");
-	jclass DcData = (*env)->GetObjectClass(env, jdcdata);
-	jmethodID getFd = (*env)->GetMethodID(env, DcData, "getFd", "()I");
-	jmethodID getVendor = (*env)->GetMethodID(env, DcData, "getVendor", "()Ljava/lang/String;");
-	jmethodID getProduct = (*env)->GetMethodID(env, DcData, "getProduct", "()Ljava/lang/String;");
-	jmethodID isForce = (*env)->GetMethodID(env, DcData, "isForce", "()Z");
-	jmethodID isPrefer = (*env)->GetMethodID(env, DcData, "isPrefer", "()Z");
-	jmethodID isLog = (*env)->GetMethodID(env, DcData, "isLog", "()Z");
-	jmethodID isDump = (*env)->GetMethodID(env, DcData, "isDump", "()Z");
-	jmethodID getLogfilepath = (*env)->GetMethodID(env, DcData, "getLogfilepath", "()Ljava/lang/String;");
-	jmethodID getOutfilepath = (*env)->GetMethodID(env, DcData, "getOutfilepath", "()Ljava/lang/String;");
-
-	jint jfd =(*env)->CallIntMethod(env, jdcdata, getFd);
-	jstring jvendor = (*env)->CallObjectMethod(env, jdcdata, getVendor);
-	jstring jproduct = (*env)->CallObjectMethod(env, jdcdata, getProduct);
-	jboolean jforce = (*env)->CallBooleanMethod(env, jdcdata, isForce);
-	jboolean jprefer = (*env)->CallBooleanMethod(env, jdcdata, isPrefer);
-	jboolean jlog = (*env)->CallBooleanMethod(env, jdcdata, isLog);
-	jboolean jdump = (*env)->CallBooleanMethod(env, jdcdata, isDump);
-	jstring jlogfilepath = (*env)->CallObjectMethod(env, jdcdata, getLogfilepath);
-	jstring joutfilepath = (*env)->CallObjectMethod(env, jdcdata, getOutfilepath);
-
-	dcdata.fd = jfd;
-	dcdata.force_download = jforce;
-	dcdata.libdc_log = jlog;
-	dcdata.libdc_dump = jdump;
-	dcdata.deviceid = 0;
-	dcdata.diveid = 0;
-
-	LOGD("file descriptor received : %d\n", jfd);
-
-	if (jvendor != NULL) {
-		const char *vndr = (*env)->GetStringUTFChars(env, jvendor, NULL);
-		char *v = (char *) malloc(STRSIZE);
-		strncpy(v, vndr, STRSIZE);
-		dcdata.vendor = v;
-		(*env)->ReleaseStringUTFChars(env, jvendor, vndr);
-	}
-	if (jproduct != NULL) {
-		const char *prdt = (*env)->GetStringUTFChars(env, jproduct, NULL);
-		char *p = (char *) malloc(STRSIZE);
-		strncpy(p, prdt, STRSIZE);
-		dcdata.product = p;
-		(*env)->ReleaseStringUTFChars(env, jproduct, prdt);
-
-	}
-
-	device_descriptor_lookup(&dcdata);
-
-	if (jlog && jlogfilepath != NULL) {
-		const char *logfp = (*env)->GetStringUTFChars(env, jlogfilepath, NULL);
-		char *lfp = (char *) malloc(PATHSIZE);
-		strncpy(lfp, logfp, PATHSIZE);
-		(*env)->ReleaseStringUTFChars(env, jlogfilepath, logfp);
-		logfile_name = lfp;
-
-	}
-	if (joutfilepath != NULL) {
-		const char *dumpfp = (*env)->GetStringUTFChars(env, joutfilepath, NULL);
-		char *ofp = (char *) malloc(PATHSIZE);
-		strncpy(ofp, dumpfp, PATHSIZE);
-		(*env)->ReleaseStringUTFChars(env, joutfilepath, dumpfp);
-		dumpfile_name = ofp;
-	}
-
-	LOGD("successfully finished init_dcdata");
-}
-
-JNIEXPORT void JNICALL do_dc_import(JNIEnv *env, jobject jobj)
-{
-	LOG_F("do_dc_import");
-	const char *error_text;
-	error_text = do_libdivecomputer_import(&dcdata);
-	if (error_text)
-		LOGD("error while import : %s\n", error_text);
-	LOGD("Finished do_dc_import\n");
-}
-
 jint JNI_OnLoad(JavaVM *vm, void *reserved)
 {
+	LOG_F("JNI_OnLoad");
 	// Get JNI Env for all function calls
 	JNIEnv* env;
 	if ((*vm)->GetEnv(vm, (void **) &env, JNI_VERSION_1_6) != JNI_OK) {
@@ -193,41 +266,36 @@ jint JNI_OnLoad(JavaVM *vm, void *reserved)
 	     return -1;
 	}
 
-	// Find the class calling native function
-	// org/libdivecomputer/ImportProgress.java
-	jclass ImportProgress = (*env)->FindClass(env, "org/libdivecomputer/ImportProgress");
-	if (ImportProgress == NULL) {
-		LOGD ("FindClass failed : org.libdivecomputer.ImportProgress\n");
+	// Find the class org/libdivecomputer/DcData containing all native
+	// functions
+	jclass DcData = (*env)->FindClass(env, "org/libdivecomputer/DcData");
+	if (DcData == NULL) {
+		LOGD ("FindClass failed : org.libdivecomputer.DcData\n");
 		return -1;
 	}
 
 	// Register native method for getUsbPermission
 	JNINativeMethod nm2[] = {
-		{ "initDcData",	"(Lorg/libdivecomputer/DcData;)V", init_dcdata },
-
+		{ "resetDcData", "()V", reset_dc_data },
+		{ "setUsbFd", "(I)I", set_usb_fd},
+		{ "setPrefer", "(Z)V", set_prefer},
+		{ "setForce", "(Z)V", set_force},
+		{ "setLog", "(Z)V", set_log},
+                { "setDump", "(Z)V", set_dump},
+                { "setVendorName", "(Ljava/lang/String;)I", set_vendor_name},
+                { "setProductName", "(Ljava/lang/String;)I", set_product_name},
+		{ "setLogFile",	"(Ljava/lang/String;)I", set_log_file },
+		{ "setDumpFile", "(Ljava/lang/String;)I", set_dump_file },
+		{ "setXmlFile",	"(Ljava/lang/String;)I", set_xml_file },
+		{ "doDcImport", "()V", do_dc_import },
+		{ "doProcessDives", "()V", do_process_dives },
+		{ "doSaveDives", "()V", do_save_dives },
 	};
 
-	if ((*env)->RegisterNatives(env, ImportProgress, nm2 , sizeof (nm2) / sizeof (nm2[0]))) {
+	if ((*env)->RegisterNatives(env, DcData, nm2 , sizeof (nm2) / sizeof (nm2[0]))) {
 	     LOGD ("RegisterNatives Failed.\n");
 	     return -1;
 	}
-
-	jclass DcImportTask = (*env)->FindClass(env, "org/libdivecomputer/ImportProgress$DcImportTask");
-	if (ImportProgress == NULL) {
-		LOGD ("FindClass failed : org.libdivecomputer.ImportProgress$DcImportTask\n");
-		return -1;
-	}
-
-	// Register native method for getUsbPermission
-	JNINativeMethod nm3[] = {
-		{ "doDcImport",	"()Z", do_dc_import },
-	};
-
-	if ((*env)->RegisterNatives(env, DcImportTask, nm3 , sizeof (nm3) / sizeof (nm3[0]))) {
-	     LOGD ("RegisterNatives Failed.\n");
-	     return -1;
-	}
-
 
 	return JNI_VERSION_1_6;
 }
